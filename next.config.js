0f4a3b37ccdbf1decb653939c9a747da798c91c5
nathanndk <Razor091203@gmail.com>
2023-11-22 18:34:10 +0700

    build the navbar

--- next.config.js
+++ next.config.js
@@ -1,4 +1,17 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {}
 
-module.exports = nextConfig
+// module.exports = nextConfig
+
+module.exports = {
+    async redirects() {
+      return [
+        {
+          source: '/',
+          destination: '/root-page',
+          permanent: false,
+        },
+      ]
+    },
+  }
+  
